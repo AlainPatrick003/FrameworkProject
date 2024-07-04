4337b80fd7bbbf1f397cd48e5cecba85c81cf4e0
AlainPatrick003 <152412702+AlainPatrick003@users.noreply.github.com>
2024-07-04 11:14:02 +0200

    sprint 8 terminé

--- src/session/CustomSession.java
+++ src/session/CustomSession.java
@@ -1,15 +1,27 @@
-package session;
+package mg.itu.prom16.session;
 
 import java.util.HashMap;
 
+import jakarta.servlet.http.HttpSession;
+
 public class CustomSession {
-    public HashMap<String, Object> values;
+    public HttpSession session;
+
+    public CustomSession(HttpSession session) {
+        this.session = session;
+    }
 
     public void add(String key, Object value) {
-        this.values.put(key, value);
-    } 
+        session.setAttribute(key, value);
+    }
 
     public void delete(String key) {
-        
+        session.removeAttribute(key);
     }
+
+    public Object get(String key) {
+        return this.session.getAttribute(key);
+    }
+
+    
 }
